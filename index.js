112e81666823656841c30b1e6e28d19f8656eecf
JClark <justindanielclark@gmail.com>
2022-09-12 13:54:57 -0700

    project finished per guidelines

--- index.js
+++ index.js
@@ -1,46 +1,57 @@
+const submitBtn = document.querySelector(`button[type="submit"]`);
 let lastKeyDown = '';
 document.addEventListener('keydown',(e)=>{
   lastKeyDown = e.key;
 });
 
-const nameRegex = new RegExp(`([A-Z,À-ÿ][-,a-z. ']+[ ]*)+`);
-const numberRegex = new RegExp(`[0-9]`);
-const upperCaseRegex = new RegExp(`[A-Z]`);
-const lowerCaseRegex = new RegExp(`[a-z]`);
-const specialCharRegex = new RegExp(`[^a-zA-Z0-9]`);
+const invalidNameCharsRegex = new RegExp(/[!@#$%&\*\(\)+={};<>?/\\]/);
+const numberRegex = new RegExp(/[0-9]/);
+const upperCaseRegex = new RegExp(/[A-Z]/);
+const lowerCaseRegex = new RegExp(/[a-z]/);
+const specialCharRegex = new RegExp(/[^a-zA-Z0-9]/);
+const validEmailRegex = new RegExp(/.*@.*/);
+const phoneNumberRegex = new RegExp(/\(\d{3}\)\d{3}\-\d{4}/);
 
 //NAME INPUT LOGIC
 const fNameInputGroup = document.querySelector(`.inputGroup[data-input="fName"]`);
   const fNameInput = fNameInputGroup.querySelector(`input`);
   const fNameLabel = fNameInputGroup.querySelector(`.labelWrapper`);
+  const fNameMessage = fNameInputGroup.querySelector(`.inputNoticeMessage`);
 const lNameInputGroup = document.querySelector(`.inputGroup[data-input="lName"]`);
   const lNameInput = lNameInputGroup.querySelector(`input`);
   const lNameLabel = lNameInputGroup.querySelector(`.labelWrapper`);
-fNameInput.addEventListener(`blur`, (e)=>toggleValidityOnBlur(fNameInput, fNameLabel))
-lNameInput.addEventListener(`blur`, (e)=>toggleValidityOnBlur(lNameInput, lNameLabel))
-function toggleValidityOnBlur(input, label){
-  if(nameRegex.test(input.value)){
-    if(!label.classList.contains(`valid`)){
-      label.classList.toggle(`valid`);
-    }
-    if(label.classList.contains(`invalid`)){
-      label.classList.toggle(`invalid`);
-    }
-  } else {
-    if(!label.classList.contains(`invalid`)){
-      label.classList.toggle(`invalid`);
-    }
-    if(label.classList.contains(`valid`)){
-      label.classList.toggle(`valid`);
-    }
-  }
-}  
+  const lNameMessage = lNameInputGroup.querySelector(`.inputNoticeMessage`);
+fNameInput.addEventListener(`blur`, (e)=>{
+  let validation = validateName(e.target.value);
+  toggleLabelValidityAndUpdateMessage(validation.valid, fNameLabel, fNameMessage, validation.message);
+  isFormReady();
+  return;
+})
+lNameInput.addEventListener(`blur`, (e)=>{
+  let validation = validateName(e.target.value);
+  toggleLabelValidityAndUpdateMessage(validation.valid, lNameLabel, lNameMessage, validation.message);
+  isFormReady();
+  return;
+})
 
+//EMAIL INPUT LOGIC
+const emailInputGroup = document.querySelector(`.inputGroup[data-input="email"]`);
+  const emailInput = emailInputGroup.querySelector(`input`);
+  const emailLabel = emailInputGroup.querySelector(`.labelWrapper`);
+  const emailMessage = emailInputGroup.querySelector(`.inputNoticeMessage`);
+  emailInput.addEventListener(`blur`, (e)=>{
+    let valid = validEmailRegex.test(e.target.value)
+    let messageContent = valid ? ``: `Please enter a valid email address`;
+    toggleLabelValidityAndUpdateMessage(valid, emailLabel, emailMessage, messageContent);
+    isFormReady();
+    return;
+  })
 
 //PHONE INPUT LOGIC
 const phoneInputGroup = document.querySelector(`.inputGroup[data-input="phone"]`);
   const phoneInput = phoneInputGroup.querySelector(`input`);
-  const phoneLabel = phoneInputGroup.querySelector(`label`);
+  const phoneLabel = phoneInputGroup.querySelector(`.labelWrapper`);
+  const phoneMessage = phoneInputGroup.querySelector(`.inputNoticeMessage`);
 const maskMap = new Map();
   maskMap.set(`0`, `(`);
   maskMap.set(`4`, `)`);
@@ -73,7 +84,8 @@ phoneInput.addEventListener('input', (e)=>{
   }
 });
 phoneInput.addEventListener(`blur`, (e)=>{
-  if(e.target.value.length === 13){
+  if(e.target.value.length === 13 && phoneNumberRegex.test(e.target.value)){
+    phoneMessage.textContent = ``;
     if(!phoneLabel.classList.contains(`valid`)){
       phoneLabel.classList.toggle(`valid`);
     }
@@ -81,6 +93,7 @@ phoneInput.addEventListener(`blur`, (e)=>{
       phoneLabel.classList.toggle(`invalid`);
     }
   } else {
+    phoneMessage.textContent = `Please enter a complete valid phone number`;
     if(!phoneLabel.classList.contains(`invalid`)){
       phoneLabel.classList.toggle(`invalid`);
     }
@@ -88,8 +101,9 @@ phoneInput.addEventListener(`blur`, (e)=>{
       phoneLabel.classList.toggle(`valid`);
     }
   }
+  isFormReady();
+  return;
 })
-
 
 //PASSWORD LOGIC 
 const passwordInputGroup = document.querySelector(`.inputGroup[data-input="password"]`);
@@ -98,25 +112,18 @@ const passwordInputGroup = document.querySelector(`.inputGroup[data-input="passw
 const confirmPasswordInputGroup = document.querySelector(`.inputGroup[data-input="passwordConfirm"]`);
   const confirmPasswordInput = confirmPasswordInputGroup.querySelector(`input`);
   const confirmPasswordLabel = confirmPasswordInputGroup.querySelector(`.labelWrapper`);
-let hasLength = false;
 const lengthMessage = document.querySelector(`.checkLength`);
-let hasUppercase = false;
 const uppercaseMessage = document.querySelector(`.checkUppercase`);
-let hasLowercase = false;
 const lowercaseMessage = document.querySelector(`.checkLowercase`);
-let hasDigit = false;
 const digitMessage = document.querySelector(`.checkDigit`);
-let hasSpecialCharacter = false;
 const specialCharacterMessage = document.querySelector(`.checkSpecialCharacter`);
-let hasMatches = false;
 const matchesMessage = document.querySelector(`.checkMatch`);
-
 passwordInput.addEventListener(`blur`, (e)=>{
-  hasLength = false;
-  hasUppercase = false;
-  hasLowercase = false;
-  hasDigit = false;
-  hasSpecialCharacter = false;
+  let hasLength = false;
+  let hasUppercase = false;
+  let hasLowercase = false;
+  let hasDigit = false;
+  let hasSpecialCharacter = false;
   let str = e.target.value;
   hasLength = str.length >= 7;
   for(let i = 0; i < str.length; i++){
@@ -134,29 +141,111 @@ passwordInput.addEventListener(`blur`, (e)=>{
       hasSpecialCharacter = specialCharRegex.test(c);
     }
   }
+  if(hasLength && hasUppercase && hasLowercase && hasDigit && hasSpecialCharacter){
+    if(passwordLabel.classList.contains(`invalid`)){passwordLabel.classList.toggle(`invalid`)}
+    if(!passwordLabel.classList.contains(`valid`)){passwordLabel.classList.toggle(`valid`)}
+  } else {
+    if(passwordLabel.classList.contains(`valid`)){passwordLabel.classList.toggle(`valid`)}
+    if(!passwordLabel.classList.contains(`invalid`)){passwordLabel.classList.toggle(`invalid`)}
+  }
+  toggleMessageValidity(hasLength, lengthMessage);
+  toggleMessageValidity(hasUppercase, uppercaseMessage);
+  toggleMessageValidity(hasLowercase, lowercaseMessage);
+  toggleMessageValidity(hasDigit, digitMessage);
+  toggleMessageValidity(hasSpecialCharacter, specialCharacterMessage);
+  isFormReady();
+  return;
+  
+})
+confirmPasswordInput.addEventListener(`blur`, (e)=>{
+  let hasMatch = confirmPasswordInput.value === passwordInput.value;
+  if(hasMatch){
+    if(confirmPasswordLabel.classList.contains(`invalid`)){confirmPasswordLabel.classList.toggle(`invalid`)}
+    if(!confirmPasswordLabel.classList.contains(`valid`)){confirmPasswordLabel.classList.toggle(`valid`)}
+  } else {
+    if(confirmPasswordLabel.classList.contains(`valid`)){confirmPasswordLabel.classList.toggle(`valid`)}
+    if(!confirmPasswordLabel.classList.contains(`invalid`)){confirmPasswordLabel.classList.toggle(`invalid`)}
+  }
+  toggleMessageValidity(hasMatch, matchesMessage);
+  isFormReady();
+  return;
+})
 
-  doToggle(hasLength, lengthMessage);
-  doToggle(hasUppercase, uppercaseMessage);
-  doToggle(hasLowercase, lowercaseMessage);
-  doToggle(hasDigit, digitMessage);
-  doToggle(hasSpecialCharacter, specialCharacterMessage);
 
-  function doToggle(isValid, message){
-    if(isValid){
-      if(!message.classList.contains(`valid`)){
-        message.classList.toggle(`valid`);
-      }
-      if(message.classList.contains(`invalid`)){
-        message.classList.toggle(`invalid`);
-      }
-    }
-    else{
-      if(!message.classList.contains(`invalid`)){
-        message.classList.toggle(`invalid`);
-      }
-      if(message.classList.contains(`valid`)){
-        message.classList.toggle(`valid`);
-      }
+
+
+function validateName(name){
+  if(!(name.length > 2 && name.length < 30)){
+    return {
+      valid: false,
+      message: `Names are between 2 and 30 characters`
     }
   }
-})
+
+  if(!upperCaseRegex.test(name.slice(0,1))){
+    return {
+      valid: false,
+      message: `Names must start with an Uppercase Letter`
+    }
+  }
+
+  let arr = [...name.matchAll(invalidNameCharsRegex.source)];
+  if(arr.length > 0){
+    arr = [... new Set(arr.map(entry => {return entry[0]}))];
+    return {
+      valid: false,
+      message: `Names cannot contain the following characters: ${arr.toString()}`
+    }
+  }
+
+  return {
+    valid: true,
+    message: ``
+  }
+}
+function toggleMessageValidity(isValid, message){
+  if(isValid){
+    if(!message.classList.contains(`valid`)){
+      message.classList.toggle(`valid`);
+    }
+    if(message.classList.contains(`invalid`)){
+      message.classList.toggle(`invalid`);
+    }
+  }
+  else{
+    if(!message.classList.contains(`invalid`)){
+      message.classList.toggle(`invalid`);
+    }
+    if(message.classList.contains(`valid`)){
+      message.classList.toggle(`valid`);
+    }
+  }
+}
+function toggleLabelValidityAndUpdateMessage(isValid, label, message, messageContent){
+  if(isValid){
+    if(label.classList.contains(`invalid`)){label.classList.toggle(`invalid`)}
+    if(!label.classList.contains(`valid`)){label.classList.toggle(`valid`)}
+  } else {
+    if(label.classList.contains(`valid`)){label.classList.toggle(`valid`)}
+    if(!label.classList.contains(`invalid`)){label.classList.toggle(`invalid`)}
+  }
+  message.textContent = messageContent;
+}
+function validateSignUpForm(event){
+  if(!submitBtn.classList.contains(`valid`)){
+    return false;
+  }
+}
+function isFormReady(){
+  let valid = fNameLabel.classList.contains(`valid`) &&
+    lNameLabel.classList.contains(`valid`) &&
+    emailLabel.classList.contains(`valid`) &&
+    phoneLabel.classList.contains(`valid`) &&
+    passwordLabel.classList.contains(`valid`) &&
+    confirmPasswordLabel.classList.contains(`valid`);
+  if(valid){
+    if(!submitBtn.classList.contains(`valid`)){submitBtn.classList.toggle(`valid`);}
+  } else {
+    if(submitBtn.classList.contains(`valid`)){submitBtn.classList.toggle(`valid`);}
+  }
+}
